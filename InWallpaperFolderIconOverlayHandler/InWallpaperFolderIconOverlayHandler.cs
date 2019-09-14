6c8628af002e039edd3b8031aac18b204e72a098
marshl <marshl@live.com.au>
2019-09-14 11:34:06 +1000

    Initial commit

---
+++ InWallpaperFolderIconOverlayHandler/InWallpaperFolderIconOverlayHandler.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using SharpShell.Attributes;
+using SharpShell.Interop;
+using SharpShell.SharpIconOverlayHandler;
+
+namespace InWallpaperFolderIconOverlayHandler
+{
+    /// <summary>
+    /// The ReadOnlyFileIconOverlayHandler is an IconOverlayHandler that shows
+    /// a padlock icon over files that are read only.
+    /// </summary>
+    [ComVisible(true)]
+    [RegistrationName("      InWallpaperFolderIconOverlayHandler")] // push our way up the list by putting spaces in the name...
+    public class InWallpaperFolderIconOverlayHandler : SharpIconOverlayHandler
+    {
+        /// <summary>
+        /// Called by the system to get the priority, which is used to determine
+        /// which icon overlay to use if there are multiple handlers. The priority
+        /// must be between 0 and 100, where 0 is the highest priority.
+        /// </summary>
+        /// <returns>
+        /// A value between 0 and 100, where 0 is the highest priority.
+        /// </returns>
+        protected override int GetPriority()
+        {
+            //  The read only icon overlay is very low priority.
+            return 90;
+        }
+
+        /// <summary>
+        /// Determines whether an overlay should be shown for the shell item with the path 'path' and
+        /// the shell attributes 'attributes'.
+        /// </summary>
+        /// <param name="path">The path for the shell item. This is not necessarily the path
+        /// to a physical file or folder.</param>
+        /// <param name="attributes">The attributes of the shell item.</param>
+        /// <returns>
+        ///   <c>true</c> if this an overlay should be shown for the specified item; otherwise, <c>false</c>.
+        /// </returns>
+        protected override bool CanShowOverlay(string path, FILE_ATTRIBUTE attributes)
+        {
+            try
+            {
+                if ((int)(attributes & FILE_ATTRIBUTE.FILE_ATTRIBUTE_DIRECTORY) == (int)FILE_ATTRIBUTE.FILE_ATTRIBUTE_DIRECTORY)
+                {
+                    return false;
+                }
+
+                //  Get the file attributes.
+                var fileAttributes = new FileInfo(path);
+
+                var parentDir = fileAttributes.Directory;
+                bool inPicturesDir = false;
+                while (parentDir != null)
+                {
+                    if (parentDir.Name == "Pictures")
+                    {
+                        inPicturesDir = true;
+                        break;
+                    }
+
+                    if (parentDir.Name == "CustomWallpapers")
+                    {
+                        return false;
+                    }
+                    parentDir = parentDir.Parent;
+                }
+
+                //  Return true if the file is read only, meaning we'll show the overlay.
+                if (!inPicturesDir)
+                {
+                    return false;
+                }
+
+                var userDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                var wallpaperDir = new DirectoryInfo(Path.Combine(userDir, "Pictures", "CustomWallpapers"));
+                var wallpaperFiles = wallpaperDir.GetFiles("*.jpg");
+                var nameWithoutExtension = Path.GetFileNameWithoutExtension(path);
+                foreach (var wallpaper in wallpaperFiles)
+                {
+                    if (Path.GetFileNameWithoutExtension(wallpaper.Name) == nameWithoutExtension)
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Called to get the icon to show as the overlay icon.
+        /// </summary>
+        /// <returns>
+        /// The overlay icon.
+        /// </returns>
+        protected override System.Drawing.Icon GetOverlayIcon()
+        {
+            //  Return the read only icon.
+            return Properties.Resources._112_Tick_Green;
+        }
+    }
+}
---
+++ WallpaperManagementExtension/WallpaperManagementExtension.cs
@@ -0,0 +1,259 @@
+﻿using SharpShell.Attributes;
+using SharpShell.SharpContextMenu;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace WallpaperManagementExtension
+{
+    /// <summary>
+    /// The CountLinesExtensions is an example shell context menu extension,
+    /// implemented with SharpShell. It adds the command 'Count Lines' to text
+    /// files.
+    /// </summary>
+    [ComVisible(true)]
+    [COMServerAssociation(AssociationType.AllFiles)]
+    public class WallpaperManagementExtension : SharpContextMenu
+    {
+        private DirectoryInfo GetWallpaperFolder()
+        {
+            var userDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            return new DirectoryInfo(Path.Combine(userDir, "Pictures", "CustomWallpapers"));
+        }
+
+        private bool IsInPictures(FileInfo file)
+        {
+            return this.IsFileInDirectory(file, "Pictures");
+        }
+
+        private bool IsInCustomWallpapers(FileInfo file)
+        {
+            return this.IsFileInDirectory(file, "CustomWallpapers");
+        }
+
+        private bool IsFileInDirectory(FileInfo file, string directoryName)
+        {
+            if (!file.Exists)
+            {
+                return false;
+            }
+
+            for (DirectoryInfo parentDir = file.Directory; parentDir != null; parentDir = parentDir.Parent)
+            {
+                if (parentDir.Name == directoryName)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool CanFileBeAdded(FileInfo file)
+        {
+            if (!this.IsInPictures(file) || this.IsInCustomWallpapers(file))
+            {
+                return false;
+            }
+
+            if (this.AcceptedWallpaperFilenames.Contains(Path.GetFileNameWithoutExtension(file.Name)))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool CanFileBeRemoved(FileInfo file)
+        {
+            if (!this.IsInPictures(file) || this.IsInCustomWallpapers(file))
+            {
+                return false;
+            }
+
+            if (this.AcceptedWallpaperFilenames.Contains(Path.GetFileNameWithoutExtension(file.Name)))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private readonly List<string> AcceptedWallpaperFilenames = new List<string>();
+
+        private void GetAcceptedWallpaperFiles()
+        {
+            var wallpaperFiles = this.GetWallpaperFolder().GetFiles();
+            foreach (var wallpaper in wallpaperFiles)
+            {
+                this.AcceptedWallpaperFilenames.Add(Path.GetFileNameWithoutExtension(wallpaper.Name));
+            }
+        }
+
+        /// <summary>
+        /// Determines whether this instance can a shell
+        /// context show menu, given the specified selected file list
+        /// </summary>
+        /// <returns>
+        /// <c>true</c> if this instance should show a shell context
+        /// menu for the specified file list; otherwise, <c>false</c>
+        /// </returns>
+        protected override bool CanShowMenu()
+        {
+            this.GetAcceptedWallpaperFiles();
+
+            foreach (string path in this.SelectedItemPaths)
+            {
+                FileInfo file = new FileInfo(path);
+                if (this.CanFileBeAdded(file) || this.CanFileBeRemoved(file))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Creates the context menu. This can be a single menu item or a tree of them.
+        /// </summary>
+        /// <returns>
+        /// The context menu for the shell context menu.
+        /// </returns>
+        protected override ContextMenuStrip CreateMenu()
+        {
+            this.GetAcceptedWallpaperFiles();
+            bool canAddFile = false;
+            bool canRemoveFile = false;
+
+            foreach (string path in this.SelectedItemPaths)
+            {
+                FileInfo file = new FileInfo(path);
+                canAddFile |= this.CanFileBeAdded(file);
+                canRemoveFile |= this.CanFileBeRemoved(file);
+            }
+
+            //  Create the menu strip
+            var menu = new ContextMenuStrip();
+
+            if (canAddFile)
+            {
+                //  Create a 'count lines' item
+                var itemAddWalpaper = new ToolStripMenuItem
+                {
+                    Text = "Add to wallpapers",
+                    //Image = Properties.Resources.CountLines
+                };
+
+                //  When we click, we'll count the lines
+                itemAddWalpaper.Click += (sender, args) => AddAsWallpaper();
+                //  Add the item to the context menu.
+                menu.Items.Add(itemAddWalpaper);
+            }
+
+            if (canRemoveFile)
+            {
+                var itemRemoveWallpaper = new ToolStripMenuItem
+                {
+                    Text = "Remove from wallpapers"
+                };
+
+                itemRemoveWallpaper.Click += (sender, args) => RemoveWallpapers();
+                menu.Items.Add(itemRemoveWallpaper);
+            }
+
+            //  Return the menu
+            return menu;
+        }
+
+        /// <summary>
+        /// Counts the lines in the selected files
+        /// </summary>
+        private void AddAsWallpaper()
+        {
+            var wallpaperFolder = this.GetWallpaperFolder();
+            //  Go through each file.
+            foreach (var filePath in SelectedItemPaths)
+            {
+                FileInfo file = new FileInfo(filePath);
+                if (this.CanFileBeAdded(file))
+                {
+                    this.MoveFileToWallpapers(file, wallpaperFolder);
+                }
+            }
+            this.RefreshOverlays();
+        }
+
+        private void MoveFileToWallpapers(FileInfo file, DirectoryInfo wallpaperFolder)
+        {
+            if (file.Extension != ".jpg")
+            {
+                string outputFileName = Path.GetFileNameWithoutExtension(file.Name) + ".jpg";
+                Process converter = new Process();
+                converter.StartInfo.FileName = @"C:\Program Files (x86)\ImageMagick-6.9.3-Q16\convert.exe";
+                converter.StartInfo.Arguments = $"\"{file.FullName}\" -resize 1920x1080 \"{Path.Combine(wallpaperFolder.FullName, outputFileName)}\"";
+                
+                var confirmResult = MessageBox.Show($"Run convert.exe {converter.StartInfo.Arguments}",
+                                     "Confirm copy",
+                                     MessageBoxButtons.YesNo);
+
+                if (confirmResult == DialogResult.Yes)
+                {
+                    converter.Start();
+                }
+            }
+            else
+            {
+                var confirmResult = MessageBox.Show($"Copy to {Path.Combine(wallpaperFolder.FullName, file.Name)}",
+                                     "Confirm copy",
+                                     MessageBoxButtons.YesNo);
+
+                if (confirmResult == DialogResult.Yes)
+                {
+                    File.Copy(file.FullName, Path.Combine(wallpaperFolder.FullName, file.Name));
+                }
+            }
+        }
+
+        private void RemoveWallpapers()
+        {
+            var wallpaperFolder = this.GetWallpaperFolder();
+            //  Go through each file.
+            foreach (var filePath in SelectedItemPaths)
+            {
+                FileInfo file = new FileInfo(filePath);
+                if (this.CanFileBeRemoved(file))
+                {
+                    this.RemoveSingleWWallpaper(file, wallpaperFolder);
+                }
+            }
+            this.RefreshOverlays();
+        }
+
+        private void RemoveSingleWWallpaper(FileInfo file, DirectoryInfo wallpaperFolder)
+        {
+            string filePathToDelete = Path.Combine(wallpaperFolder.FullName, Path.GetFileNameWithoutExtension(file.Name) + ".jpg");
+            var confirmResult = MessageBox.Show($"Are you sure you want to remove {filePathToDelete}?",
+                                     "Confirm deletion",
+                                     MessageBoxButtons.YesNo);
+
+            if (confirmResult == DialogResult.Yes)
+            {
+                File.Delete(filePathToDelete);
+            }
+        }
+
+        private void RefreshOverlays()
+        {
+            Process ie4uinit = new Process();
+            ie4uinit.StartInfo.FileName = "ie4uinit.exe";
+            ie4uinit.StartInfo.Arguments = "-show";
+            ie4uinit.Start();
+        }
+    }
+}
